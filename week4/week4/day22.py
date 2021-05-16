a5c4bd81aaeaac3817881b310daca007f29bcb13
Akshith <akshith.isola@gmail.com>
2021-05-16 16:07:38 +0530

    snake game

---
+++ week4/week4/day22.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+import turtle as t
+my_screen = Screen()
+my_screen.bgcolor('black')
+my_screen.setup(width = 800, height = 600)
+
+
+
+
+
+my_screen.exitonclick()
